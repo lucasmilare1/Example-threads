ab94f3f2ad6cdcf3c8b7fe4132bedb21a39a1ef3
lucasmilare1 <65171151+lucasmilare1@users.noreply.github.com>
2021-04-06 11:23:11 -0300

    Ajuste tarefa

--- src/br/com/banheiro/Principal.java
+++ src/br/com/banheiro/Principal.java
@@ -6,9 +6,9 @@ public class Principal {
         Banheiro banheiro = new Banheiro();
 
         Thread convidade1 = new Thread(new TarefaNumero1(banheiro),"João");
-        Thread convidade2 = new Thread(new TarefaNumero1(banheiro),"Pedro");
+        Thread convidade2 = new Thread(new TarefaNumero2(banheiro),"Pedro");
         Thread convidade3 = new Thread(new TarefaNumero1(banheiro),"Cleide");
-        Thread convidade4 = new Thread(new TarefaNumero1(banheiro),"Cleiton");
+        Thread convidade4 = new Thread(new TarefaNumero2(banheiro),"Cleiton");
 
         convidade1.start();
         convidade2.start();
